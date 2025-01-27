4ad2bf0a4daef2c42740c9c49f45becfebb04528
Y7REDSBOY <y.alsanea@outlook.com>
2025-01-28 01:24:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function updateClock(){
 
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById("myclock").textContent = timeString;
+
+}
+
+updateClock()
+setInterval(updateClock, 1000);
